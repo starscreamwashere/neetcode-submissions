84337b363fc2f00c21fab6b4d30f858c5869ef98
starscreamwashere <119345619+starscreamwashere@users.noreply.github.com>
2026-06-24 12:52:55 +0530

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(2 * n); // Initialize ans with size 2n
+        
+        for (int i = 0; i < 2 * n; i++) {
+            if (i >= n) {
+                ans[i] = nums[i - n];
+            } else {
+                ans[i] = nums[i];
+            }
+        }
+        
+        return ans;
+    }
+};
