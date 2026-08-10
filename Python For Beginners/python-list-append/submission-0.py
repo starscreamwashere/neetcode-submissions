46ea7da7442316d9b6042ccd7e4ba52da18cec19
starscreamwashere <119345619+starscreamwashere@users.noreply.github.com>
2026-08-10 12:37:03 +0530

    Add: python-list-append - submission-0

---
+++ Python For Beginners/python-list-append/submission-0.py
@@ -0,0 +1,10 @@
+from typing import List # this is used to add type hints for List type
+
+def append_to_list(my_list: List[int], elements: List[int]) -> List[int]:
+    return my_list.append(elements)
+
+
+
+# do not modify below this line
+print(append_to_list([1, 2, 3], [4, 5]))
+print(append_to_list([], [1, 2, 3, 4]))
