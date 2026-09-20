110b91a6d7f38fd55105dad946830e15e18a2344
starscreamwashere <119345619+starscreamwashere@users.noreply.github.com>
2026-09-21 00:02:16 +0530

    Add: reverse-integer - submission-0

---
+++ Data Structures & Algorithms/reverse-integer/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        temp=x
+        place_value=int(math.log10(abs(x)))
+        rev=0
+        x=abs(x)
+        while x>0:
+            digit=x%10
+            rev+=digit*(10**place_value)
+            place_value-=1
+            x//=10
+        if temp<0:
+            rev*=-1
+        if rev<(-2**31) or rev >(2**31)-1:
+            return 0
+        return rev
+
+
+        
